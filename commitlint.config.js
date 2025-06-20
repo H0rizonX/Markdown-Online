297a2b5d7d4e34e04989b5b81dc9580538d15894
H0rizonX <1572121126@qq.com>
2025-06-20 18:15:55 +0800

    feat: 增加hotfix类型

--- commitlint.config.js
+++ commitlint.config.js
@@ -5,7 +5,7 @@ module.exports = {
     "type-enum": [
       2,
       "always",
-      ["feat", "fix", "docs", "style", "refactor", "test", "chore"],
+      ["feat", "fix", "docs", "style", "refactor", "test", "chore", "hotfix"],
     ],
     "type-empty": [2, "never", "提交类型不能为空！"],
     "subject-empty": [2, "never", "提交说明不能为空！"],
