55b2c236254befc8869d2c3c17deba89d5b4bad1
H0rizonX <1572121126@qq.com>
2025-11-20 18:07:16 +0800

    fix: 删除部分临时文件

--- apps/markdown2html-server/src/controller/tempCodeRunnerFile.ts
+++
@@ -1,5 +0,0 @@
-router.get("/create", async (req: Request, res: Response) => {
-  const data = await articleService.createRoom();
-  console.log("用户创建房间");
-  return res.suc(data);
-});
--- apps/markdown2html-server/src/service/collabManager.ts
+++ apps/markdown2html-server/src/service/collabManager.ts
@@ -17,4 +17,3 @@ export const ensureCollabServer = (): CollabService | null => {
 };
 
 export const getCollabServer = (): CollabService | null => instance;
-
